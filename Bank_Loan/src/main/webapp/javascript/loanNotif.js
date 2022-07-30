002b03a75473aba5a855e19865ad265ff9c440c9
mrrotijohn <mrrotijohn@live.com>
2022-07-30 15:42:21 +0800

    updated javascript for loan notification
    clearer console message

--- Bank_Loan/src/main/webapp/javascript/loanNotif.js
+++ Bank_Loan/src/main/webapp/javascript/loanNotif.js
@@ -7,6 +7,9 @@
 						console.log(day);
 						alert('Reminder to make your payment for your loan balance.');
 					}
+					else {
+						console.log("It is day "+ day + " of the month, not the time to remind loan payment.")
+					}
 				}
 			catch (e) {
 				console.log(e);
